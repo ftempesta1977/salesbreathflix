e3bdb84bd990557035a811145d0b9904881421c2
Luiz Fabiano Tempesta <luiz.tempesta@personalsoft.com.br>
2020-07-31 17:38:13 -0300

    heroku

--- server.js
+++ server.js
@@ -9,6 +9,5 @@ const port = process.env.PORT || 8080;
 server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
-  /* eslint-disable no-console */
   console.log(`JSON Server is running in ${port}`);
 });
